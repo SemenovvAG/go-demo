9d4fcd52cbfcc7027e9648461f003791e3ad16ce
SemenovvAG <semenov.anrdey@gmail.com>
2025-11-27 17:20:36 +0300

    Сделал калькулятор ИМТ

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 
-	fmt.Printf("Привет")
+	var userHeight = 1.83
+	var userKg float64 = 81
+	var IMT = userKg / math.Pow(userHeight, 2)
+	fmt.Print(IMT)
 }
