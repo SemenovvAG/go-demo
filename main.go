ca3e65bd9316ae29d26080a6536ad49c17fd0a82
SemenovvAG <semenov.anrdey@gmail.com>
2025-11-28 12:47:57 +0300

    Добавил ввод данных

--- main.go
+++ main.go
@@ -9,6 +9,11 @@ func main() {
 
 	var userHeight = 1.83
 	var userKg float64 = 81
+	fmt.Print("__ Калькулятор индекса массы тела __\n")
+	fmt.Print("Введите свой рост: ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Введите свой вес: ")
+	fmt.Scan(userKg)
 	var IMT = userKg / math.Pow(userHeight, 2)
 	fmt.Print(IMT)
 }
